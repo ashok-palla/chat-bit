d0ee20a82c57bbb2a2feacd2d427f4e7f144a815
Ashok Palla <ashok_palla@yahoo.com>
2018-03-14 22:59:46 +0530

    commit

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ restService.post("/audio", function (req, res) {
 
 restService.post("/video", function (req, res) {
   return res.json({
-    speech: '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    speech: '<speak>  <audio src="http://123telugump3.net/load/A-to-Z/A/Abhimanyudu-(2018)//.48Kbps/01_-_Adige_(From_Abhimanyudu).mp3">did not get your MP3 audio file</audio></speak>',
     displayText: '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
     source: "webhook-echo-sample"
   });
