8e7aad93d9f119f4b34cc3e3a2a0ba535b142b72
Ashok Palla <ashok_palla@yahoo.com>
2018-03-15 13:48:48 +0530

    _

--- dataLayer.js
+++ dataLayer.js
@@ -7,11 +7,11 @@ var connection = mysql.createConnection({
     connectTimeout: 30000
 });
 module.exports.employees = function (emploeeId, callback) {
-    // connection.connect();
+     connection.connect();
     // connection.query("SELECT CONCAT(FirstName, CONCAT(' ', LastName)) as name  FROM employee WHERE ID =" + emploeeId, function (error, results, fields) {
     //     if (error) throw error;
     //     callback(results[0].name);
     // });
-    // connection.end();
+    connection.end();
     callback("Ashok Palla");
 };
