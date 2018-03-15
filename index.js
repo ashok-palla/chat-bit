97ff6b7543bf8fdec9b9f4acac8d4d06b4176204
Ashok Palla <ashok_palla@yahoo.com>
2018-03-15 11:52:40 +0530

    changes

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ restService.use(bodyParser.urlencoded({ extended: true }));
 restService.use(bodyParser.json());
 var data_layer = require('./dataLayer');
 restService.post('/employee', function (req, res) {
-  console.log(req.body);
   var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.employeeId ? req.body.result.parameters.employeeId : "Seems like some problem. Speak again.";
   //console.log(req.body.result.parameters.employeeId);
   // return res.json({ speech: speech, displayText: speech, source: "meritus-bot" });
