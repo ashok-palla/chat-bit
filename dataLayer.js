dda7ef17f35875d120d6a603795fe82b3eabc145
Ashok Palla <ashok_palla@yahoo.com>
2018-03-16 00:05:29 +0530

    finally working

--- dataLayer.js
+++ dataLayer.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'us-cdbr-iron-east-05.cleardb.net',
-    user: 'b6dc78df8aae06',
-    password: '4a0ec4f7',
-    database: 'heroku_f9ca64a925836b6',
-    connectTimeout: 30000
-});
 module.exports.employees = function (emploeeId, callback) {
+    var connection = mysql.createConnection({
+        host: 'us-cdbr-iron-east-05.cleardb.net',
+        user: 'b6dc78df8aae06',
+        password: '4a0ec4f7',
+        database: 'heroku_f9ca64a925836b6',
+        connectTimeout: 30000
+    });
     connection.connect();
     connection.query("SELECT CONCAT(FirstName, CONCAT(' ', LastName)) as name  FROM employee WHERE ID =" + emploeeId, function (error, results, fields) {
         if (error) throw error;
