78dcb9be11c6feeb265cba8b2924e28a9b3701ae
Ashok Palla <ashok_palla@yahoo.com>
2018-03-15 11:32:13 +0530

    test

--- dataLayer.js
+++ dataLayer.js
@@ -14,3 +14,11 @@ module.exports.employees = function (emploeeId, callback) {
     });
     connection.end();
 };
+module.exports.employees_rest = function (emploeeId, callback) {
+    connection.connect();
+    connection.query("SELECT CONCAT(FirstName, CONCAT(' ', LastName)) as name  FROM employee WHERE ID =" + emploeeId, function (error, results, fields) {
+        if (error) throw error;
+        callback(results[0].name);
+    });
+    connection.end();
+};
