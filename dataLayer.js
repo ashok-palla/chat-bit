d62cb2ddcbaac40bbe31fbe80a848dd630cc1f33
ashok-palla <ashok_palla@yahoo.com>
2018-03-16 10:27:29 +0530

    _

--- dataLayer.js
+++ dataLayer.js
@@ -13,7 +13,7 @@ module.exports.employees = function (emploeeId, callback) {
             connectTimeout: 30000
         });
         connection.connect();
-        connection.query("SELECT FirstName + ' ' LastName as name  FROM employee WHERE ID =" + emploeeId, function (error, results, fields) {
+        connection.query("SELECT FirstName + ' ' + LastName as name  FROM employee WHERE ID =" + emploeeId, function (error, results, fields) {
             if (error) throw error;
             connection.end();
             callback(results.length > 0 ? results[0].name : ('no employee exists on ' + emploeeId));
