b707ec49d33058545af14cc9f3670c4ab0ff0d6c
ashok-palla <ashok_palla@yahoo.com>
2018-03-16 09:04:17 +0530

    data type validation added

--- dataLayer.js
+++ dataLayer.js
@@ -1,17 +1,23 @@
 var mysql = require('mysql');
+var joi = require('joi');
 module.exports.employees = function (emploeeId, callback) {
-    var connection = mysql.createConnection({
-        host: 'us-cdbr-iron-east-05.cleardb.net',
-        user: 'b6dc78df8aae06',
-        password: '4a0ec4f7',
-        database: 'heroku_f9ca64a925836b6',
-        connectTimeout: 30000
-    });
-    connection.connect();
-    connection.query("SELECT FirstName as name  FROM employee WHERE ID =" + emploeeId, function (error, results, fields) {
-        if (error) throw error;
-        connection.end();
-        console.log(results);
-        callback(results.length > 0 ? results[0].name : ('no employee exists on ' + emploeeId));                 
-    });
+    const schema = { emploeeId: Joi.number() };
+    const value = { emploeeId: emploeeId };
+    Joi.validate(value, schema, (err, value) => {
+        if (err) callback('buddy only numbers allowed.');
+        var connection = mysql.createConnection({
+            host: 'us-cdbr-iron-east-05.cleardb.net',
+            user: 'b6dc78df8aae06',
+            password: '4a0ec4f7',
+            database: 'heroku_f9ca64a925836b6',
+            connectTimeout: 30000
+        });
+        connection.connect();
+        connection.query("SELECT FirstName as name  FROM employee WHERE ID =" + emploeeId, function (error, results, fields) {
+            if (error) throw error;
+            connection.end();
+            console.log(results);
+            callback(results.length > 0 ? results[0].name : ('no employee exists on ' + emploeeId));
+        });
+     });
 };
