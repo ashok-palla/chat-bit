10bdb7f77c2eca07ecad2889a717b6d60d18fa86
Ashok Palla <ashok_palla@yahoo.com>
2018-03-17 16:45:56 +0530

    A

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ restService.use(function (req, res, next) { next(); });
 restService.post('/meritus_bot', function (req, res) {
   if (req.body.result.metadata.intentName === "whose_employee_id") {
     if(req.body.result && req.body.result.parameters && req.body.result.parameters.employeeId === "" && req.body.result.parameters.employeeName === "" && req.body.result.parameters.lastName === ""){
-      return res.status(200).json({ speech: 'some of the times i am very difficult to find your name, please try different way', displayText: 'some of the times i am very difficult to find your name, please try different way', source: "meritus-bot" });
+      return res.status(200).json({ speech: 'some of the times i am very difficult to find your question, please try different way', displayText: 'some of the times i am very difficult to find your question, please try different way', source: "meritus-bot" });
     }
     else if (req.body.result && req.body.result.parameters && req.body.result.parameters.employeeId) {
       data_layer.employeeId(req.body.result.parameters.employeeId, (results) => {
