dcf4f83c081964eb5fdad134888e9f624d7fe4e5
ashok-palla <ashok_palla@yahoo.com>
2018-03-19 13:32:11 +0530

    rich text

--- index.js
+++ index.js
@@ -75,9 +75,7 @@ restService.post('/meritus_bot', function (req, res) {
                   {
                     "basicCard": {
                       "title": (results[0].FirstName + ' ' + results[0].LastName).toLocaleUpperCase(),
-                      "formattedText": "**First Name:** " + results[0].FirstName + ""
-                        + "**Last Name:** " + results[0].FirstName + ""
-                        + "**Designation:** " + results[0].Designation + "",
+                      "formattedText": "**First Name:** " + results[0].FirstName,
                       "image": {
                         "url": "https://c1.staticflickr.com/3/2841/11529684343_8dd9c26cff.jpg",
                         "accessibilityText": (results[0].FirstName + ' ' + results[0].LastName).toLocaleLowerCase()
