0025d645da0c7b7cace99080a8c34936a0d8bd83
ashok-palla <ashok_palla@yahoo.com>
2018-03-19 14:27:09 +0530

    rich text

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ restService.post('/meritus_bot', function (req, res) {
                     "basicCard": {
                       "title": (results[0].FirstName + ' ' + results[0].LastName),
                       "subtitle": results[0].Designation,
-                      "formattedText": "**First Name:** " + results[0].FirstName
-                        + "\n**Last Name:** " + results[0].LastName,
+                      "formattedText": "**First Name:** " + results[0].FirstName + "\n"
+                        + "**Last Name:** " + results[0].LastName,
                       "image": {
                         "url": "https://c1.staticflickr.com/3/2841/11529684343_8dd9c26cff.jpg",
                         "accessibilityText": (results[0].FirstName + ' ' + results[0].LastName)
@@ -125,8 +125,8 @@ restService.post('/meritus_bot', function (req, res) {
                       "basicCard": {
                         "title": (results[0].FirstName + ' ' + results[0].LastName),
                         "subtitle": results[0].Designation,
-                        "formattedText": "**First Name:** " + results[0].FirstName
-                          + "\n**Last Name:** " + results[0].LastName,
+                        "formattedText": "**First Name:** " + results[0].FirstName + "\n"
+                          + "**Last Name:** " + results[0].LastName,
                         "image": {
                           "url": "https://c1.staticflickr.com/3/2841/11529684343_8dd9c26cff.jpg",
                           "accessibilityText": (results[0].FirstName + ' ' + results[0].LastName)
