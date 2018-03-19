23a5e7c3e18c6b6517ebf40f20ec7911475702dc
Ashok Palla <ashok_palla@yahoo.com>
2018-03-19 11:36:37 +0530

    image added

--- index.js
+++ index.js
@@ -187,77 +187,69 @@ restService.post('/meritus_bot', function (req, res) {
             source: "meritus-bot",
             "data": {
               "google": {
-                "conversationToken": "",
                 "expectUserResponse": true,
-                "expectedInputs": [{
-                  "inputPrompt": {
-                    "initialPrompts": [{
-                      "textToSpeech": "Alright! Here are a few things you can learn. Which sounds interesting?"
-                    }],
-                    "noInputPrompts": []
-                  },
-                  "possibleIntents": [{
-                    "intent": "actions.intent.OPTION",
-                    "inputValueData": {
-                      "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                      "carouselSelect": {
-                        "items": [{
-                            "optionInfo": {
-                              "key": "MATH_AND_PRIME",
-                              "synonyms": [
-                                "math",
-                                "math and prime",
-                                "prime numbers",
-                                "prime"
-                              ]
-                            },
-                            "title": "Math & prime numbers",
-                            "description": "42 is an abundant number because the sum of its proper divisors 54 is greater…",
-                            "image": {
-                              "url": "http://example.com/math_and_prime.jpg",
-                              "accessibilityText": "Math & prime numbers"
-                            }
-                          },
-                          {
-                            "optionInfo": {
-                              "key": "EGYPT",
-                              "synonyms": [
-                                "religion",
-                                "egpyt",
-                                "ancient egyptian"
-                              ]
-                            },
-                            "title": "Ancient Egyptian religion",
-                            "description": "42 gods who ruled on the fate of the dead in the afterworld. Throughout the under…",
-                            "image": {
-                              "url": "http://example.com/egypt",
-                              "accessibilityText": "Egypt"
-                            }
-                          },
-                          {
-                            "optionInfo": {
-                              "key": "RECIPES",
-                              "synonyms": [
-                                "recipes",
-                                "recipe",
-                                "42 recipes"
-                              ]
-                            },
-                            "title": "42 recipes with 42 ingredients",
-                            "description": "Here's a beautifully simple recipe that's full of flavor! All you need is some ginger and…",
-                            "image": {
-                              "url": "http://example.com/recipe",
-                              "accessibilityText": "Recipe"
-                            }
-                          }
-                        ]
-                      }
+                "richResponse": {
+                  "items": [{
+                    "simpleResponse": {
+                      "textToSpeech": 'oh there is ' + results.length + ' ' + req.body.result.parameters.employeeName + '\'s check the list'
                     }
-                  }]
-                }]
+                  }],
+                  "carouselSelect": {
+                    "items": [{
+                        "optionInfo": {
+                          "key": "MATH_AND_PRIME",
+                          "synonyms": [
+                            "math",
+                            "math and prime",
+                            "prime numbers",
+                            "prime"
+                          ]
+                        },
+                        "title": "Math & prime numbers",
+                        "description": "42 is an abundant number because the sum of its proper divisors 54 is greater…",
+                        "image": {
+                          "url": "http://example.com/math_and_prime.jpg",
+                          "accessibilityText": "Math & prime numbers"
+                        }
+                      },
+                      {
+                        "optionInfo": {
+                          "key": "EGYPT",
+                          "synonyms": [
+                            "religion",
+                            "egpyt",
+                            "ancient egyptian"
+                          ]
+                        },
+                        "title": "Ancient Egyptian religion",
+                        "description": "42 gods who ruled on the fate of the dead in the afterworld. Throughout the under…",
+                        "image": {
+                          "url": "http://example.com/egypt",
+                          "accessibilityText": "Egypt"
+                        }
+                      },
+                      {
+                        "optionInfo": {
+                          "key": "RECIPES",
+                          "synonyms": [
+                            "recipes",
+                            "recipe",
+                            "42 recipes"
+                          ]
+                        },
+                        "title": "42 recipes with 42 ingredients",
+                        "description": "Here's a beautifully simple recipe that's full of flavor! All you need is some ginger and…",
+                        "image": {
+                          "url": "http://example.com/recipe",
+                          "accessibilityText": "Recipe"
+                        }
+                      }
+                    ]
+                  }
+                }
               },
               "slack": {
-                "text": 'oh there is ' + results.length + ' ' + req.body.result.parameters.employeeName + '\'s check the list'
+                "text": result.speech
               },
             }
           });
