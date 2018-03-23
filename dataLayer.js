d37582a97c7c94edb70c3baf9ff9b58ffa7c808e
ashok_palla <ashok_palla@yahoo.com>
2018-03-24 02:11:25 +0530

    role added

--- dataLayer.js
+++ dataLayer.js
@@ -164,12 +164,12 @@ module.exports.employeeSearch = function (params, callback) {
             connection.query("SELECT E.*, r.role_name FROM employee E JOIN roles r ON r.role_id = E.RoleID WHERE E.FirstName like '%" + params.lastName + "%' or E.LastName like '%" + params.firstName + "%'", function (error, results, fields) {
                 connection.end();
                 if (error) callback('buddy, \nplease check employee identification.');
-                console.log(params);
                 var RResult = JSON.parse(JSON.stringify(results));
+                console.log(RResult);
                 if(RResult.length === 0){
-                    callback('sorry date of joining not exists');
+                    callback('sorry email not exists');
                 }
-                callback((RResult[0].FirstName + ' ' + RResult[0].LastName).toLocaleLowerCase() + ' ' + params.employee_search_criteria + ' is ' + new Date(RResult[0].Date_of_Joining));
+                callback((RResult[0].FirstName + ' ' + RResult[0].LastName).toLocaleLowerCase() + ' ' + params.employee_search_criteria + ' is ' + RResult[0].EmailID);
             });
         }
         else {
