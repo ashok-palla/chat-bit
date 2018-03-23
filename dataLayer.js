d2f830175ca98d80e51ed2454b24decd05800d3d
ashok_palla <ashok_palla@yahoo.com>
2018-03-24 01:59:01 +0530

    role added

--- dataLayer.js
+++ dataLayer.js
@@ -130,6 +130,34 @@ module.exports.employeeSearch = function (params, callback) {
                 callback((RResult[0].FirstName + ' ' + RResult[0].LastName).toLocaleLowerCase() + ' ' + params.employee_search_criteria + ' is ' + RResult[0].ID);
             });
         }
+        else if (params.employee_search_criteria === 'status') {
+            var connection = mysql.createConnection(credentials);
+            connection.connect();
+            connection.query("SELECT E.*, r.role_name FROM employee E JOIN roles r ON r.role_id = E.RoleID WHERE E.FirstName like '%" + params.lastName + "%' or E.LastName like '%" + params.firstName + "%'", function (error, results, fields) {
+                connection.end();
+                if (error) callback('buddy, \nplease check employee identification.');
+                console.log(params);
+                var RResult = JSON.parse(JSON.stringify(results));
+                if(RResult.length === 0){
+                    callback('sorry status not exists');
+                }
+                callback((RResult[0].FirstName + ' ' + RResult[0].LastName).toLocaleLowerCase() + ' ' + params.employee_search_criteria + ' is ' + RResult[0].status ? ' Active' : 'Inactive');
+            });
+        }
+        else if (params.employee_search_criteria === 'date of join') {
+            var connection = mysql.createConnection(credentials);
+            connection.connect();
+            connection.query("SELECT E.*, r.role_name FROM employee E JOIN roles r ON r.role_id = E.RoleID WHERE E.FirstName like '%" + params.lastName + "%' or E.LastName like '%" + params.firstName + "%'", function (error, results, fields) {
+                connection.end();
+                if (error) callback('buddy, \nplease check employee identification.');
+                console.log(params);
+                var RResult = JSON.parse(JSON.stringify(results));
+                if(RResult.length === 0){
+                    callback('sorry date of joining not exists');
+                }
+                callback((RResult[0].FirstName + ' ' + RResult[0].LastName).toLocaleLowerCase() + ' ' + params.employee_search_criteria + ' is ' + RResult[0].Date_of_Joining);
+            });
+        }
         else {
             callback('buddy, still i am in development mode');
         }
