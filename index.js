d791f74997fb96f972bedeae25fd0931f50a2a10
ashok_palla <ashok_palla@yahoo.com>
2018-03-24 15:40:48 +0530

    shift pc

--- index.js
+++ index.js
@@ -23,7 +23,13 @@ process.on('uncaughtException', function (err) {
 restService.post('/meritus_bot', function (req, res) {
   if (req.body.result.metadata.intentName === "shift_my_pc - checking_employee_ID") {
     data_layer.shift_my_pc(req.body.result.contexts[3].parameters, (results) => {
-      console.log(results.name);
+      if (results[''] === '') {
+        return res.status(200).json({
+          speech: 'I couldn\'t find your employee ID, can you please repeat it again',
+          displayText: 'I couldn\'t find your employee ID, can you please repeat it again',
+          source: "meritus-bot"
+        });
+      }
       mailer.sendMail({
         to: '"' + (results.name).toLocaleLowerCase() + '" <' + results.emailID + '>',
         subject: results.emailSubject,
